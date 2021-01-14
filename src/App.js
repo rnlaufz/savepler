a8013d935f666659348ce5bea8d6b84b931cdc43
Regina Zaripova <rnloftz@gmail.com>
2021-01-14 16:33:14 +1000

    created  markup and styling for navigation | settled general styling & some utils

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-     <h2>Savepler</h2>
+    <div className="container">
+      <div id="content-container" className="pos-flex-split">
+        <div id="nav-box">
+          <h2 className="title-sm">Name</h2>
+          <ul id="nav-list">
+            <li><a href="#">Goals</a></li>
+            <li><a href="#">Saving History</a></li>
+            <li><a href="#">Statictics</a></li>
+          </ul>
+          <div id="nav-footer">
+            <ul id="nav-footer-list" className="pos-flex-split">
+              <li><a href="#">About</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+            <p>&copy;All Rights Reserved
+              <br/>
+              Build by /RZ</p>
+          </div>
+        </div>
+        <div id="main-box"></div>
+      </div>
     </div>
   );
 }
