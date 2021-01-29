1e9b021554102492e7c4128bb1fd5f36c2a35f84
Regina Zaripova <rnloftz@gmail.com>
2021-01-29 18:12:49 +1000

    added user registration and logging in funcs

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(express.json({extended: false}));
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes 
-// app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/tasks', require('./routes/api/goals'));
 
 const PORT = process.env.PORT || 5000;
