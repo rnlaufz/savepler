50928d4681e7f0e53e9a413a10332fe138c07212
Regina Zaripova <rnloftz@gmail.com>
2021-02-22 19:23:06 +1000

    added history navigations buttons | history buttons are avaliable if there are more then 16 records in array of history

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import React, {Fragment, useEffect} from 'react';
 import {v4 as uuid} from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 
 import { getAllRecords } from "../actions/history";
 import HistoryItem from './HistoryItem';
@@ -8,7 +10,9 @@ import HistoryItem from './HistoryItem';
  const History = ({getAllRecords, history:{allRecords}}) => {
     useEffect(() => {
         getAllRecords()
-    }, [getAllRecords, allRecords])
+    }, [getAllRecords, allRecords]);
+    const numRecords = allRecords.length;
+    console.log(numRecords)
     return (
         <Fragment>
         <div className="history-container pos-flex">
@@ -16,6 +20,23 @@ import HistoryItem from './HistoryItem';
             <ul className="history-list">
             {allRecords.map((record) => (<HistoryItem id={uuid()} key={uuid()} record={record} />))}
             </ul>
+            {numRecords > 16 ? ( <div className="history-nav-btns">
+                <button className="history-nav-btn">
+                    <FontAwesomeIcon icon={faChevronLeft}/>
+                    <FontAwesomeIcon icon={faChevronLeft}/>
+                </button>
+                <button className="history-nav-btn">
+                <FontAwesomeIcon icon={faChevronLeft}/>
+                </button>
+                <button className="history-nav-btn">
+                <FontAwesomeIcon icon={faChevronRight}/>
+                </button>
+                <button className="history-nav-btn">
+                <FontAwesomeIcon icon={faChevronRight}/>
+                <FontAwesomeIcon icon={faChevronRight}/>
+                </button>
+            </div>) : null }
+           
         </div>
        </Fragment>
     )
