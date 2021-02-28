8517ef4cb4b76c832546137e3030955ba2f827b6
Regina Zaripova <rnloftz@gmail.com>
2021-02-28 18:01:55 +1000

    added history item reaction on currency user has chosen: ruble is no longer default option

--- client/src/components/HistoryItem.js
+++ client/src/components/HistoryItem.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import Moment from 'react-moment'
-;
+import React, {Fragment} from 'react';
+import Moment from 'react-moment';
  const HistoryItem = ({record: {action, amount, currency, date}}) => {
     return (
-        <li className="history-item"><p className="pos-flex-split"><span className="sign">{action === 'add' ? "+" : "-"}</span> <span className="sum">{amount} <span>&#8381;</span></span><span className="date"><Moment format="YYYY.MM.DD" date={date} /></span></p></li>
+        <li className="history-item"><p className="pos-flex-split"><span className="sign">{action === 'add' ? "+" : action === "lend" ? "-" : action}</span> <span className="sum">{amount} <span>{currency === "RUB" ? <Fragment>&#8381;</Fragment>: currency === "USD" ? <Fragment>&#36;</Fragment> : currency === "EUR" ? <Fragment>&#8364;</Fragment> : null}</span></span><span className="date"><Moment format="YYYY.MM.DD" date={date} /></span></p></li>
     )
 }
 
