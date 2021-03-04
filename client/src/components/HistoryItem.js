379eda3e7982fb4bea373db321ca12c16681e065
Regina Zaripova <rnloftz@gmail.com>
2021-03-04 18:50:42 +1000

    finished dasboard adaptive styling: changed navigation sizes, fixed history action item, fixed add input, fixed action form

--- client/src/components/HistoryItem.js
+++ client/src/components/HistoryItem.js
@@ -4,7 +4,19 @@ import propTypes from 'prop-types';
 
  const HistoryItem = ({record: {action, amount, currency, date}}) => {
     return (
-        <li className="history-item"><p className="pos-flex-split"><span className="sign">{action === 'add' ? "+" : action === "lend" ? "-" : <span className="history-action">{action}</span> }</span> <span className="sum">{action !== "add" && action !== "lend" ? null : amount} <span>{action !== "add" && action !== "lend" ? null : currency === "RUB" ? <Fragment>&#8381;</Fragment>: currency === "USD" ? <Fragment>&#36;</Fragment> : currency === "EUR" ? <Fragment>&#8364;</Fragment> : null}</span></span><span className="date"><Moment format="YYYY.MM.DD" date={date} /></span></p></li>
+        <li className="history-item">
+            <p className="pos-flex-split">
+                <span className="sign">
+                    {action === 'add' ? "+" : action === "lend" ? "-" : 
+                <span className="history-action">{action}</span>}</span> 
+                <span className="sum">{action !== "add" && action !== "lend" ? null : amount} 
+                <span>{action !== "add" && action !== "lend" ? null : currency === "RUB" ? 
+                <Fragment> &#8381;</Fragment> : currency === "USD" ? 
+                <Fragment> &#36;</Fragment> : currency === "EUR" ? 
+                <Fragment> &#8364;</Fragment> : null}</span></span>
+                <span className="date"><Moment format="YYYY.MM.DD" date={date} /></span>
+                </p>
+                </li>
     )
 }
 
